65d8252a7b315a03e96305571e8bfa999e1d874d
keithmoore <keithmoore@unomaha.edu>
2021-10-18 15:44:10 -0500

    admin.py

--- catalog/models.py
+++ catalog/models.py
@@ -1,7 +1,8 @@
 from django.contrib.auth.models import User
 from django.db import models
 
-class Category(models.Model): #14min in
+
+class Category(models.Model):  # 14min in
     name = models.CharField(max_length=255, db_index=True)
     slug = models.SlugField(max_length=255, unique=True)
 
@@ -11,10 +12,11 @@ class Category(models.Model): #14min in
     def get_absolute_url(self):
         return self.name
 
-class Product(models.Model): #about 19 min in
+
+class Product(models.Model):  # about 19 min in
     category = models.ForeignKey(Category, related_name='product', on_delete=models.CASCADE)
-    created_by = models.ForeignKey(User, on_delete= models.CASCADE, related_name='product_creator')
-    title = models.CharField(max_length= 255)
+    created_by = models.ForeignKey(User, on_delete=models.CASCADE, related_name='product_creator')
+    title = models.CharField(max_length=255)
     author = models.CharField(max_length=255, default='admin')
     description = models.TextField(blank=True)
     image = models.ImageField(upload_to='images/')
@@ -27,9 +29,7 @@ class Product(models.Model): #about 19 min in
 
     class Meta:
         verbose_name_plural = 'Products'
-        ordering = ('-created',) #27:21 #can remove hyphen to have all "created" orders show
+        ordering = ('-created',)  # 27:21 #can remove hyphen to have all "created" orders show
 
     def __str__(self):
-        return self.title #im at 34:26 10/5/2021
-
-
+        return self.title  # im at 34:26 10/5/2021
