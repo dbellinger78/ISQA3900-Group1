0bdeaad1568b66de76e0a9ae771d7230eb8aeeab
Dustin Bellinger <77249898+dbellinger78@users.noreply.github.com>
2021-11-17 18:34:37 -0600

    Add files via upload

---
+++ estore/views.py
@@ -0,0 +1,18 @@
+from django.shortcuts import get_object_or_404, render
+from .models import Category, Product
+
+
+def product_all(request):
+    products = Product.products.all()
+    return render(request, 'estore/home.html', {'products': products})
+
+
+def product_detail(request, slug):
+    product = get_object_or_404(Product, slug=slug, in_stock=True)
+    return render(request, 'estore/products/detail.html', {'product': product})
+
+
+def category_list(request, category_slug):
+    category = get_object_or_404(Category, slug=category_slug)
+    products = Product.objects.filter(category=category)
+    return render(request, 'estore/products/category.html', {'category': category, 'products': products})
